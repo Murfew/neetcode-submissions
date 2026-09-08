cef988113a42fd730582bfd65c96039f3c3a9e92
Matthew Hurley <matthew.hurley953@gmail.com>
2026-09-08 16:41:00 -0400

    Add: car-fleet - submission-11

---
+++ Data Structures & Algorithms/car-fleet/submission-11.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pairs = [(p, s) for p, s in zip(position, speed)]
+        pairs.sort(reverse=True)
+
+        fleets = 1
+
+        prevTime = (target - pairs[0][0]) / pairs[0][1]
+        for p, s in pairs[1:]:
+            time = (target - p) / s
+
+            if time > prevTime:
+                fleets += 1
+                prevTime = time
+
+        return fleets
